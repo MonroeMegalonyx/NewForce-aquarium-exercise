396e6cdae555d6586c71545b5d396215c48e9f3e
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-12 13:55:10 -0500

    Finished Chapter 8 'Automated Fish Making Machine' to add fish script

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,13 @@
+/*
+Your job is to define what a JavaScript object would look like. What keys should you create on each fish object? Each fish will have the same keys, but the state of each will be different - meaning the keys will have different values.
+ */
+
+const fishCollection = [
+  {
+    species: "Beta Fish",
+    name: "George",
+    length: "2 inches",
+    food: "pretzels",
+    location: "Galapagos Island",
+  },
+];
